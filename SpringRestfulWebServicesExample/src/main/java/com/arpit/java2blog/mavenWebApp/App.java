0dd7e4dbca2190ed55870709277cdea84570cfce
Deuja <Deuja@DESKTOP-CVCNRRG>
2016-11-06 18:27:03 -0600

    Initial commit

---
+++ SpringRestfulWebServicesExample/src/main/java/com/arpit/java2blog/mavenWebApp/App.java
@@ -0,0 +1,13 @@
+package com.arpit.java2blog.mavenWebApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
